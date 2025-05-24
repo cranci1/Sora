5ce9a7b00132f0444beb30a7784891a2553b51b9
Seiike <122684677+Seeike@users.noreply.github.com>
2025-05-24 21:59:55 +0200

    Update EpisodeCell.swift

--- Sora/Views/MediaInfoView/EpisodeCell/EpisodeCell.swift
+++ Sora/Views/MediaInfoView/EpisodeCell/EpisodeCell.swift
@@ -108,11 +108,9 @@ struct EpisodeCell: View {
             episodeThumbnail
             episodeInfo
             Spacer()
-            if module.metadata.type?.lowercased() == "anime" {
-                CircularProgressBar(progress: currentProgress)
-                    .frame(width: 40, height: 40)
-                    .padding(.trailing, 8)
-            }
+            CircularProgressBar(progress: currentProgress)
+                .frame(width: 40, height: 40)
+                .padding(.trailing, 8)
         }
         .contentShape(Rectangle())
         .background(isMultiSelectMode && isSelected ? Color.accentColor.opacity(0.1) : Color.clear)

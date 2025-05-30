bed4be2fc1e7845b86dafe43fe77f32efa62895c
50/50 <80717571+50n50@users.noreply.github.com>
2025-05-31 00:12:28 +0200

    IBH special 💯

--- Sora/Views/SettingsView/SettingsSubViews/SettingsViewAbout.swift
+++ Sora/Views/SettingsView/SettingsSubViews/SettingsViewAbout.swift
@@ -218,7 +218,7 @@ struct ContributorView: View {
                 
                 Text(contributor.login)
                     .font(.headline)
-                    .foregroundColor(.accentColor)
+                    .foregroundColor(contributor.login == "IBH-RAD" ? Color(hexTwo: "#41127b") : .accentColor)
                 
                 Spacer()
                 Image(systemName: "safari")
@@ -239,5 +239,31 @@ struct Contributor: Identifiable, Decodable {
         case id
         case login
         case avatarUrl = "avatar_url"
+    }
+}
+
+extension Color {
+    init(hexTwo: String) {
+        let hexTwo = hexTwo.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hexTwo).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hexTwo.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
     }
 }
